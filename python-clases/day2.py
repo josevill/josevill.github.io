0859c818ac9ed4d00593f38ccce9577de1abcde1
Jose Manuel Viloria <joseviloria@parceiro.sonae.pt>
2019-03-06 13:12:32 +0100

    Clases Py

---
+++ python-clases/day2.py
@@ -0,0 +1,8 @@
+print('Dia 2')
+print('''Tenemos String, Integers, FloatPoint y Bool.
+Cada linea a partir de este punto va a ser compuesta por elementos primitivos de esos cuatro tipos y un chequeo de tipo.
+''')
+print('String => Esta misma linea es un string, largo')
+print('Tambien pueden ser largos')
+
+print('')
---
+++ python-clases/day3.py
@@ -0,0 +1,8 @@
+if 5+2 > 10:
+  print("Es mayor que 5.")
+elif 10-5 == 5:
+  print("Es igual a 5")
+else:
+  print("No es mayor que 5")
+
+print("Ya el programa terminó")
